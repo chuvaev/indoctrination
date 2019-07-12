af96e9a65fe6ce8d0a1c36f4755c6b5bddc29feb
Ilya Chuvaev <Ilya_Chuvaev@epam.com>
2019-07-12 17:36:50 +0300

    create project structure

---
+++ src/main/java/com/ilyachuvaev/indoctrination/IndoctrinationApplication.java
@@ -0,0 +1,13 @@
+package com.ilyachuvaev.indoctrination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoctrinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoctrinationApplication.class, args);
+	}
+
+}
